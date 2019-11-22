5190374099f227d58f476d69a18f75d26b3d3399
Victor Berland <vb@victorberland.com>
2019-11-23 00:39:00 +0100

    init

---
+++ pages/hvordan.vue
@@ -0,0 +1,53 @@
+<template>
+  <div class="meld">
+		<div class="circle">
+			<div class="circle-content">
+				<h1>Early bird</h1>
+				<h1>299,- kr</h1>
+			</div>
+		</div>
+		<Back/>
+  </div>
+</template>
+
+<script>
+import Back from '~/components/Back.vue'
+
+export default {
+  components: {
+    Back
+  },
+	data() {
+		return {
+		}
+	}
+}
+</script>
+
+<style lang="scss">
+.meld {
+	.circle {
+		background: white;
+		mix-blend-mode: screen;
+		position: absolute;
+		width: 100vh;
+		height: 100vh;
+		border-radius: 100%;
+		left: 50%;
+		top: 50%;
+		transform: translate(-50%,-50%);
+		.circle-content {
+			position: absolute;
+			top: 50%;
+			left: 50%;
+			width: 100%;
+			transform: translate(-50%,-50%);
+			text-align: center;
+		}
+		h1 {
+			font-size: 6em;
+			color: #000;
+		}
+	}
+}
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,52 @@
+<template>
+  <div class="front">
+		<div class="front-content">
+			<h1 @mouseenter="frontMessage = frontMessageHover" @mouseleave="frontMessage = frontMessageOrig"><nuxt-link to="/hvordan">{{ frontMessage }}</nuxt-link></h1>
+		</div>
+		<Back/>
+  </div>
+</template>
+
+<script>
+import Back from '~/components/Back.vue'
+
+export default {
+  components: {
+    Back
+  },
+	data() {
+		return {
+			frontMessage: '',
+			frontMessageOrig: '14. - 16. februar',
+			frontMessageHover: 'Meld deg på'
+		}
+	},
+	mounted() {
+		this.frontMessage = this.frontMessageOrig
+	}
+}
+</script>
+
+<style lang="scss">
+.front {
+	.side-info {
+		position: absolute;
+		bottom: 5px;
+		right: 5px;
+		color: white;
+	}
+	.front-content {
+		position: absolute;
+		top: 50%;
+		left: 50%;
+		transform: translate(-50%,-50%);
+		color: white;
+		h1 {
+			font-size: 5vw;
+			opacity: 1;
+			color: #111;
+		}
+	}
+
+}
+</style>
