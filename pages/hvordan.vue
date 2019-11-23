ed32af73c5d209fdb294e0d5a943f59fdd8efa04
Victor Berland <vb@victorberland.com>
2019-11-23 16:35:06 +0100

    div

--- pages/hvordan.vue
+++ pages/hvordan.vue
@@ -1,10 +1,20 @@
 <template>
   <div class="meld">
-		<div class="circle">
+		<div class="circle" :class="{ 'big-circle': bigCircle }">
 			<div class="circle-content">
 				<h1>Early bird</h1>
 				<h1>299,- kr</h1>
-				<p @click="goCheckout">Til checkout</p>
+				<form>
+					Antall
+					<input type="number" v-model="numberParticipants"/>
+					Alder
+					<input type="number" v-model="userAge"/>
+					<div class="underage-consent" v-if="!overAge">
+						For å delta på Glitcher LAN må foreldrene dine samtykke til at du drar. Er dette tilfellet?
+						<input type="checkbox" v-model="parentsConsent"/>
+					</div>
+				</form>
+				<p @click="goCheckout" v-if="overAge || parentsConsent">Til checkout</p>
 			</div>
 		</div>
 		<Back/>
@@ -25,6 +35,23 @@ export default {
   },
 	data() {
 		return {
+			bigCircle: false,
+			userAge: '',
+			parentsConsent: false,
+			numberParticipants: 1
+		}
+	},
+	computed: {
+		overAge() {
+			var ageLimit = 13
+
+			if (this.userAge <= ageLimit) {
+				return false
+			} else if (this.userAge > ageLimit) {
+				return true
+			} else {
+				return false
+			}
 		}
 	},
 	methods: {
@@ -34,34 +61,47 @@ export default {
 			stripe.redirectToCheckout({
 				items: [
 					// Replace with the ID of your SKU
-					{sku: 'sku_GEEOtn0fRdwg48', quantity: 1}
+					{sku: 'sku_GEEOtn0fRdwg48', quantity: Number(this.numberParticipants)}
 				],
 				successUrl: 'https://example.com/success',
 				cancelUrl: 'https://example.com/cancel',
-				customerEmail: 'customer@example.com',
+				// customerEmail: 'customer@example.com',
 			}).then(function (result) {
   			// If `redirectToCheckout` fails due to a browser or network
   			// error, display the localized error message to your customer
   			// using `result.error.message`.
 			});
 		}
+	},
+	mounted() {
+		var that = this
+
+		setTimeout(function() {
+			that.bigCircle = true
+		}, 300);
 	}
 }
 </script>
 
 <style lang="scss">
 .meld {
+	.big-circle {
+		width: 100vh!important;
+		height: 100vh!important;
+	}
 	.circle {
 		background: white;
+		transition: 1s ease;
 		mix-blend-mode: screen;
 		position: absolute;
-		width: 100vh;
-		height: 100vh;
+		width: 200px;
+		height: 200px;
 		border-radius: 100%;
 		left: 50%;
 		top: 50%;
 		transform: translate(-50%,-50%);
 		.circle-content {
+			display: none;
 			position: absolute;
 			top: 50%;
 			left: 50%;
--- pages/index.vue
+++ pages/index.vue
@@ -1,8 +1,21 @@
 <template>
   <div class="front">
-		<div class="front-content">
-			<h1 @mouseenter="frontMessage = frontMessageHover" @mouseleave="frontMessage = frontMessageOrig"><nuxt-link to="/hvordan">{{ frontMessage }}</nuxt-link></h1>
-		</div>
+
+		<div class="cursor-circle" :class="{ 'cursor-scale-1': cursorScale1, 'cursor-scale-2': cursorScale2, 'cursor-center': cursorCenter }"></div>
+
+		<transition name="fade">
+			<div class="front-content" v-show="contentShow" @mouseenter="cursorScale2 = true" @mouseleave="cursorScale2 = false">
+				<h1 @mouseenter="frontMessage = frontMessageHover" @mouseleave="frontMessage = frontMessageOrig" @click="joinClicked">{{ frontMessage }}</h1>
+			</div>
+		</transition>
+
+		<transition name="fade">
+			<header v-show="contentShow">
+				<h2>Glitcher</h2>
+				<p @mouseenter="cursorScale1 = true" @mouseleave="cursorScale1 = false">Hva er Glitcher?</p>
+			</header>
+		</transition>
+
 		<Back/>
   </div>
 </template>
@@ -18,22 +31,95 @@ export default {
 		return {
 			frontMessage: '',
 			frontMessageOrig: '14. - 16. februar',
-			frontMessageHover: 'Meld deg på'
+			frontMessageHover: 'ka vente du på?',
+			cursorScale1: false,
+			cursorScale2: false,
+			cursorCenter: false,
+			contentShow: true
 		}
 	},
 	mounted() {
 		this.frontMessage = this.frontMessageOrig
+
+		let cursorCircle = document.querySelector('.cursor-circle')
+		document.onmousemove = (event) => this.moveCursor(event, cursorCircle);
+	},
+	methods: {
+		moveCursor(event, cursorDot) {
+			console.log(event)
+			var xpos = event.clientX;
+			var ypos = event.clientY;
+			// var wheight = window.innerHeight;
+			// var wwidth = window.innerWidth;
+
+			// overlay.style.left = (wwidth / 20) + xpos / 1.2 +'px';
+			// overlay.style.top = (wheight / 25) + ypos / 1.2 +'px';
+
+			cursorDot.style.left = xpos +'px';
+			cursorDot.style.top = ypos +'px';
+		},
+		joinClicked() {
+			this.contentShow = false
+			this.cursorCenter = true
+
+			var that = this
+			setTimeout(function() {
+				that.$router.push('/hvordan')
+			}, 1000)
+		}
 	}
 }
 </script>
 
 <style lang="scss">
 .front {
-	.side-info {
+	.cursor-circle {
 		position: absolute;
-		bottom: 5px;
-		right: 5px;
+		left: 0;
+		top: 0;
+		width: 30px;
+		height: 30px;
+		background: white;
+		border-radius: 100%;
+		transition: 0.2s ease;
+		transform: translate(-50,-50%);
+		mix-blend-mode: normal;
+		z-index: 10;
+		pointer-events: none;
+	}
+	.cursor-scale-1 {
+		transform: translate(-50%,-50%) scale(3);
+		mix-blend-mode: difference;
+	}
+	.cursor-scale-2 {
+		transform: translate(-50%,-50%) scale(4);
+		mix-blend-mode: difference;
+	}
+	.cursor-center {
+		top: 50%!important;
+		left: 50%!important;
+		width: 200px;
+		height: 200px;
+		transform: translate(-50%,-50%) scale(1);
+	}
+
+
+	header {
 		color: white;
+		position: absolute;
+		bottom: 10px;
+		padding: 50px;
+		width: 100%;
+		h2 {
+			position: absolute;
+			bottom: 35px;
+		}
+		p {
+			position: absolute;
+			right: 0;
+			transform: rotate(-90deg) translate(50%,0);
+			font-size: 20px;
+		}
 	}
 	.front-content {
 		position: absolute;
@@ -43,13 +129,19 @@ export default {
 		color: white;
 		h1 {
 			font-size: 5vw;
-			a {
-				color: #efefef;
-				text-decoration: none;
-				border-bottom: 8px solid #efefef;
-			}
+			color: #efefef;
+			border-bottom: 12px solid #efefef;
+			padding-bottom: 10px;
 		}
 	}
 
+}
+
+
+.fade-enter-active, .fade-leave-active {
+  transition: opacity 0.4s;
+}
+.fade-enter, .fade-leave-to /* .fade-leave-active below version 2.1.8 */ {
+  opacity: 0;
 }
 </style>
