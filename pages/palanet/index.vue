020e87e1d907d43b0103076bab539d945f160d1e
Victor Berland <vb@victorberland.com>
2020-02-13 20:44:40 +0100

    div

--- pages/palanet/index.vue
+++ pages/palanet/index.vue
@@ -1,16 +1,30 @@
 <template>
-  <div class="palanet">
+	<div>
+		<div class="palanet">
 
-		<ul>
-			<li><nuxt-link to="/palanet/plass">Velg plass</nuxt-link></li>
-			<li><nuxt-link to="/palanet/turnering">Turnering</nuxt-link></li>
-		</ul>
+			<section class="sidebar">
+				<ul>
+					<li><nuxt-link to="/palanet/plass">Velg plass</nuxt-link></li>
+					<li><nuxt-link to="/palanet/turnering">Meld på turnering</nuxt-link></li>
+				</ul>
+			</section>
+
+			<main class="main">
+				<h1>På lanet</h1>
+			</main>
+
+		</div>
+
+
+		<div class="bg"></div>
+
 
   </div>
 </template>
 
 <script>
 export default {
+	layout: 'minimal',
 	data() {
 		return {
 
@@ -25,61 +39,93 @@ export default {
 <style lang="scss" scoped>
 
 .palanet {
-	margin-left: 8%;
-	margin-right: 5%;
-	margin-top: 8%;
-	ul {
-		list-style: none;
-		padding: 0;
-		margin: 0;
-		li {
-			margin-bottom: 30px;
-			a {
-				color: transparent;
-				mix-blend-mode: screen;
-				font-weight: bold;
-				text-transform: uppercase;
-				-webkit-text-stroke: 1px white;
-				font-size: 66px;
-
-			}
-		} 
-	}
-	h1 {
-			}
-	h3 {
-		margin-top: 50px;
-		color: white;
-		max-width: 700px;
-		line-height: 180%;
-		margin-bottom: 50px;
-	}
-	.button {
-		border: 2px double white;
-		background: transparent;
-		padding: 10px 20px;
-		color: white;
-		text-transform: uppercase;
-		letter-spacing: 0.05em;
-		cursor: pointer;
-	}
-	.content {
-		max-width: 800px;
-		color: white;
-		input[type="text"], input[type="number"], input[type="email"] {
-			width: 100%;
-			outline: none;
-			border: 0px;
-			border: 2px solid white;
+	margin: 5%;
+	.sidebar {
+		background: black;
+		padding: 80px;
+		width: 30%;
+		float: left;
+		ul {
+			list-style: none;
+			padding: 0;
+			margin: 0;
+			li {
+				margin-bottom: 50px;
+				a {
+					color: #777;
+					mix-blend-mode: screen;
+					font-weight: bold;
+					text-transform: uppercase;
+					font-size: 26px;
+					text-decoration: none;
+					transition: 0.1s color ease;
+					&:hover {
+						color: #aaa;
+					}
+				}
+			} 
+		}
+		h1 {
+				}
+		h3 {
+			margin-top: 50px;
 			color: white;
-			background: black;
+			max-width: 700px;
+			line-height: 180%;
+			margin-bottom: 50px;
+		}
+		.button {
+			border: 2px double white;
+			background: transparent;
 			padding: 10px 20px;
-			font-size: 16px;
-			margin: 20px 0;
+			color: white;
+			text-transform: uppercase;
+			letter-spacing: 0.05em;
+			cursor: pointer;
+		}
+		.content {
+			max-width: 800px;
+			color: white;
+			input[type="text"], input[type="number"], input[type="email"] {
+				width: 100%;
+				outline: none;
+				border: 0px;
+				border: 2px solid white;
+				color: white;
+				background: black;
+				padding: 10px 20px;
+				font-size: 16px;
+				margin: 20px 0;
+			}
 		}
 	}
+	.main {
+		width: 65%;
+		color: white;
+		background: black;
+		float: right;
+		padding: 100px 10%;
+		h1 {
+			-webkit-text-stroke: 1px white;
+			text-transform: uppercase;
+			color: transparent;
+			font-size: 50px;
+			letter-spacing: 5px;
+		}
+		p {
+			max-width: 700px;
+		}
+	}	
 }
 
+.bg {
+	position: fixed;
+	height: 100%;
+	width: 100%;
+	background: #261340;
+	z-index: -1;
+	top: 0;
+}
 @media(max-width: 800px) {
 	.success {
 		h1 {
---
+++ pages/turnering.vue
@@ -0,0 +1,224 @@
+<template>
+  <div class="front">
+
+		<transition name="fade">
+		</transition>
+
+		<transition name="fade">
+			<header class="header">
+				<nuxt-link to="/" class="home clickable" v-show="contentReady">
+					<img src="/logo-2.svg"/>
+				</nuxt-link>
+				<div class="header-right">
+					<p id="headerinfo">14 - 16 februar, Kopervik VGS</p>
+					<nuxt-link to="/blimed" class="join clickable" v-show="contentReady">Bli med.</nuxt-link>
+				</div>
+			</header>
+		</transition>
+
+		<transition name="fade">
+			<h1 class="big-text" v-show="blackColReady">Turnering.</h1>
+		</transition>
+		<div class="black-col" :class="{ 'loaded-black': blackColReady }">
+				<ul class="games">
+					<li>Fortnite</li>
+					<li>CS:GO</li>
+					<li>Super Smash</li>
+				</ul>
+			</div>
+
+
+		<transition name="fade">
+			<div class="content" v-show="contentReady" v-html="content.content">
+			</div>
+		</transition>
+
+  </div>
+</template>
+
+<script>
+export default {
+	data() {
+		return {
+			blackColReady: false,
+			contentReady: false,
+			content: []
+		}
+	},
+	mounted() {
+		this.getData()
+
+
+		var that = this
+
+		setTimeout(function() {
+			that.blackColReady = true
+		}, 300);
+
+		setTimeout(function() {
+			that.contentReady = true
+		}, 700);
+	},
+	methods: {
+		async getData() {
+   		const content = await this.$axios.get('/singletons/get/tournaments')
+    	this.content = content.data
+  	}
+	}
+}
+</script>
+
+<style lang="scss">
+.front {
+
+	.header {
+		position: fixed;
+		top: 0;
+		width: calc(100% - (60px * 2);
+		//right: 0;
+		margin: 45px 60px;
+		//-webkit-text-stroke: 1px white;
+		//text-transform: uppercase;
+		font-size: 20px;
+		//letter-spacing: 0.1em;
+		color: white;
+		text-decoration: none;
+		z-index: 5;
+		.home {
+			width: 180px;
+			display: block;
+			float: left;
+			margin-top: -45px;
+		}
+		.header-right {
+			float: right;
+			* {
+				float: left;
+				margin: 0 30px;
+				&:last-child {
+					margin-right: 0;
+				}
+			}
+			p {
+				opacity: 0.7;
+			}
+			.join {
+				color: white;
+				text-decoration: none;
+				padding-bottom: 6px;
+				border-bottom: 2px solid white;
+			}
+		}
+	}
+	.big-text {
+		position: absolute;
+		top: 10vh;
+		left: 10%;
+		font-size: 8vw;
+		-webkit-text-stroke: 1px white;
+		//text-shadow: -2px 2px 0 #fff, 2px 2px 0 #fff, 2px -2px 0 #fff, -2px -2px 0 #fff;
+		mix-blend-mode: lighten;
+		text-transform: uppercase;
+		letter-spacing: 0.1em;
+		line-height: 120%;
+		text-indent: 9vw;
+    z-index: 1;
+	}
+	.loaded-black {
+		transform: translateX(0)!important;
+	}
+	.black-col {
+		width: 40%;
+		min-height: 100vh;
+		background: black;
+		transform: translateX(-100%);
+		transition: 1s transform ease;
+		color: white;
+		padding: 70px;
+		mix-blend-mode: color-burn;
+		position: fixed;
+		.games {
+			position: absolute;
+			bottom: 50px;
+			list-style: none;
+			margin: 0;
+			padding: 0;
+			li {
+				font-size: 5vw;
+				font-weight: bold;
+				text-transform: uppercase;
+				user-select: none;
+				pointer-events: none;
+				margin: 2vw 0;
+			}
+		}
+	}
+	.content {
+		position: absolute;
+		left: 47%;
+		color: white;
+		top: 60vh;
+		width: 45%;
+		float: right;
+		padding-bottom: 150px;
+		h1 {
+			font-size: 62px;
+			margin: 50px 0;
+			color: white;
+			mix-blend-mode: difference;
+		}
+		p {
+			max-width: 670px;
+			line-height: 220%;
+			font-size: 16px;
+		}
+		img {
+			width: 130%;
+			height: auto;
+			margin: 100px 0;
+		}
+		ul {
+			margin-top: 20px;
+			li {
+				padding: 10px 0;
+			}
+		}
+		a {
+				color: white;
+				border-bottom: 3px solid purple;
+				text-decoration: none;
+			}
+	}
+}
+
+.fade-enter-active, .fade-leave-active {
+  transition: opacity 0.3s;
+}
+.fade-enter, .fade-leave-to /* .fade-leave-active below version 2.1.8 */ {
+  opacity: 0;
+}
+
+@media(max-width: 900px) {
+	.home {
+		display: none;
+	}
+	.front {
+		#headerinfo {
+			display: none;
+		}
+		.big-text {
+			font-size: 12vw;
+		}
+		.black-col {
+			//display: none;
+			width: 50%;
+		}
+		.content {
+			left: 0;
+			top: 70vw;
+			width: 100%;
+			padding: 0 10%;	
+		}
+	}
+}
+</style>
